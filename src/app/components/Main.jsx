f3c37113eb39145c0f2cfa40b5a9b917c6245f78
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2019-09-09 22:38:54 -0400

    front end partly complete

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/history';
+import { ConnectedNavigation } from './Navigation';
+import { ConnectTaskDetail } from './TaskDetail';
 
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-            {/*Dashboard Goes Here*/}
-            <ConnectedDashboard/>
-        </div>
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <div>
+                <ConnectedNavigation />
+                {/*<ConnectedDashboard/>*/}
+                <Route 
+                    exact 
+                    path="/dashboard"
+                    render={ () => (<ConnectedDashboard />) } 
+                />
+                <Route 
+                    exact 
+                    path="/task/:id"
+                    render={ ({match}) => (<ConnectTaskDetail match={match}/>) } 
+                />
+            </div>
+        </Provider>
+    </Router>
+
 )
